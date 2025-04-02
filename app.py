e5168e58f999798ea2ba853eea6956366e4a83a4
VerocaLim <156183349+VerocaLim@users.noreply.github.com>
2025-04-02 08:44:53 +0000

    1

--- app.py
+++ app.py
@@ -26,6 +26,10 @@ def sentiment_result():
     q = request.form.get("q")
     r = textblob.TextBlob(q).sentiment
     return(render_template("sentiment_result.html",r=r)) 
+
+@app.route("/paynow", methods=["GET","POST"]) 
+def paynow(): 
+    return(render_template("paynow.html")) 
  
 if __name__ == "__main__": 
     app.run()
